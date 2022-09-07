fdcdd8ddc3189b9d80685e8c722d92a2f76e6920
TheaKornmo <112161558+TheaKornmo@users.noreply.github.com>
2022-09-07 11:11:47 +0200

    Forandring

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ public class Hello
 {
   public static void main(String[] args)
   {
+    String helloWorld[] = {"H", "e", "l", "l", "o"};
+    for(int i = 0; i < helloWorld.length; i++)  {
+      string str = helloWorld[i];
+    }
     System.out.println("Hello world");
   }
 }
